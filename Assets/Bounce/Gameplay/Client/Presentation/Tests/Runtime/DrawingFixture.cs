d63dbdeefba7a62dfd34880340d50a54bf67234e
Ángel Siendones Sillero <culoextremo@gmail.com>
2023-01-19 12:41:04 +0100

    test: pull members up

--- Assets/Bounce/Gameplay/Client/Presentation/Tests/Runtime/DrawingFixture.cs
+++ Assets/Bounce/Gameplay/Client/Presentation/Tests/Runtime/DrawingFixture.cs
@@ -10,9 +10,6 @@ namespace Bounce.Gameplay.Presentation.Tests.Runtime
 {
     public abstract class DrawingFixture : InSceneFixture
     {
-        protected GameObject player0;
-        protected GameObject player1;
-
         protected DrawingInput drawingInput;
         protected LineRenderer lineRenderer;
 
@@ -20,25 +17,9 @@ namespace Bounce.Gameplay.Presentation.Tests.Runtime
         public override IEnumerator LoadScene()
         {
             yield return base.LoadScene();
-            player0 = GameObject.Find("player0");
-            player1 = GameObject.Find("player1");
+
             drawingInput = player0.GetComponentInChildren<DrawingInput>(true);
             lineRenderer = player0.GetComponentInChildren<LineRenderer>(true);
-        }
-
-        protected DrawingInput DrawingInputOf(GameObject player)
-        {
-            return player.GetComponentInChildren<DrawingInput>(true);
-        }
-        
-        protected LineRenderer LineRendererOf(GameObject player)
-        {
-            return player.GetComponentInChildren<LineRenderer>(true);
-        }
-        
-        protected TMP_Text TextOf(GameObject player)
-        {
-            return player.GetComponentInChildren<PointsText>(true).GetComponentInChildren<TMP_Text>(true);
         }
     }
 }
--- Assets/Bounce/Gameplay/Client/Presentation/Tests/Runtime/InSceneFixture.cs
+++ Assets/Bounce/Gameplay/Client/Presentation/Tests/Runtime/InSceneFixture.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Threading.Tasks;
+using Bounce.Gameplay.Input.Runtime;
+using Bounce.Gameplay.Presentation.Tests.Runtime.Bounce.Gameplay.Presentation.Runtime;
 using FluentAssertions;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
@@ -10,13 +13,20 @@ namespace Bounce.Gameplay.Presentation.Tests.Runtime
 {
     public abstract class InSceneFixture
     {
+        protected GameObject player0;
+        protected GameObject player1;
+        
         [UnitySetUp]
         public virtual IEnumerator LoadScene()
         {
             SceneManager.LoadScene("MainScene");
             yield return null;
             yield return null;
+            player0 = GameObject.Find("player0");
+            player1 = GameObject.Find("player1");
+            
             yield return new WaitForSeconds(3f);
+            
         }
 
         public IEnumerator AssertThatHappensInTime(Func<bool> operation, float timeOut)
@@ -30,6 +40,21 @@ namespace Bounce.Gameplay.Presentation.Tests.Runtime
             }
 
             operation().Should().BeTrue("TimeOut");
+        }
+        
+        protected DrawingInput DrawingInputOf(GameObject player)
+        {
+            return player.GetComponentInChildren<DrawingInput>(true);
+        }
+        
+        protected LineRenderer LineRendererOf(GameObject player)
+        {
+            return player.GetComponentInChildren<LineRenderer>(true);
+        }
+        
+        protected TMP_Text TextOf(GameObject player)
+        {
+            return player.GetComponentInChildren<PointsText>(true).GetComponentInChildren<TMP_Text>(true);
         }
     }
 }
